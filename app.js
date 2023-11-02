6db7af430555afbca9ffe87d2a7629a4db6e0b15
achintya-dev-repo <achintyasolutions.webdev@gmail.com>
2023-11-02 20:57:08 +0530

    add k

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-
-const express = require('express')
-const app = express()
-const port = 3000
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const puppeteer = require('puppeteer');
 
-// midleware
-app.use(cors())
-app.use(express.json())
+// Use environment variable for port or default to 3000
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.post('/submit', async (req, res) => {
-    const userInfo = req.body;
-    console.log(userInfo);
-  
-    try {
-      // const browser = await puppeteer.launch();
-    
-      const browser = await puppeteer.launch({ headless: 'new' });
-  
-      const page = await browser.newPage();
-  
-      await page.goto('https://acumens.crm.acumensinc.com/webform/view/652d6d2996eaf5.63189600');
-  
-      // Fill out the form
-      // NOTE: You'll have to replace the selectors with actual ones from the webpage.
-      await page.type('#lead_company_name', userInfo.CompanyName); // Replace '#firstNameInput' with actual selector from the CRM form
-      await page.type('#lead_firstname', userInfo.userFirstName);
-      await page.type('#lead_lastname', userInfo.userLastName);
-      await page.type('#lead_email', userInfo.userEmail);
-      await page.type('#lead_phone', userInfo.userPhnNo)
-  
-      // Submit the form
-      await page.click('#submitButton');
-  
-      await browser.close();
-  
-      res.json({ message: 'Form submitted successfully!' });
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
+  const userInfo = req.body;
+  console.log(userInfo);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      // Additional args and options for server environments (you may need to adjust based on your server setup)
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://acumens.crm.acumensinc.com/webform/view/652d6d2996eaf5.63189600');
+
+    // Fill out the form
+    await page.type('#lead_company_name', userInfo.CompanyName);
+    await page.type('#lead_firstname', userInfo.userFirstName);
+    await page.type('#lead_lastname', userInfo.userLastName);
+    await page.type('#lead_email', userInfo.userEmail);
+    await page.type('#lead_phone', userInfo.userPhnNo);
+
+    // Submit the form
+    await page.click('#submitButton');
+
+    await browser.close();
+
+    res.json({ message: 'Form submitted successfully!' });
+  } catch (error) {
+    console.error('Error submitting form:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
